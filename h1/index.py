229ecc58e272774eed350dda004e10d838ff385c
junaaxya <arjunabangka12@gmail.com>
2024-09-27 11:05:38 +0700

    tes

---
+++ h1/index.py
@@ -0,0 +1,12 @@
+nama_depan= 'Arjuna'
+nama_belakang=' '
+nama_lengkap=nama_depan+' '+nama_belakang
+usia='12'
+alamat='permis'
+kata_mutiara=' jangan berhenti berlari ayo kejar mimpi'
+print(nama_lengkap,'('+usia+')', ',' , 'dari',alamat,'kata mutiara:', kata_mutiara)
+
+print('\ntipe data dari nama_lengkap', type(nama_lengkap))
+print('tipe data dari usia', type(usia))
+print('tipe data dari usia', type(alamat))
+print('tipe data dari usia', type(kata_mutiara))
