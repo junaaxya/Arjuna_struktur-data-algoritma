b75f3136defd9e01c1b94abe0e994c6b91623465
junaaxya <arjunabangka12@gmail.com>
2024-10-11 08:32:16 +0700

    tes

---
+++ h2/index.py
@@ -0,0 +1,19 @@
+print("--------------------------")
+print("-----welcome to kasir-----")
+print("--------------------------")
+
+# kode_barang = input("masukan kode barang yang mau di jual misal nya! 223311")
+
+barang_jual = ['madu', 'kue', 'pisang',"indomie",'ayam']
+
+# barang_jual.append('kayu')
+# barang_jual.pop()
+# print(barang_jual)
+
+# index =[1,3]
+# barang = [barang_jual[i] for i in index]
+# barang_jual.remove("kue")
+# del barang_jual[2]
+# print(barang_jual[1], barang_jual[3])
+barang_jual.sort()
+print(barang_jual)
